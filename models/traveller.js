49d6a2a0c4563190226a78e1ff29b9141be5d332
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-05 16:01:42 +0100

    getJourneysByTransport created and tested

--- models/traveller.js
+++ models/traveller.js
@@ -15,7 +15,7 @@ return this.journeys.map(journey => journey.transport);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
