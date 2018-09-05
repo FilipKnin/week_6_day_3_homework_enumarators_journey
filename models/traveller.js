aaea403c9f06444b75e6febcbf8d08912cb12460
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-05 16:12:44 +0100

    calculateTotalDistanceTravelled created and tested

--- models/traveller.js
+++ models/traveller.js
@@ -19,13 +19,11 @@ return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
 return this.journeys.filter(journey => journey.distance > minDistance);
-
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+return this.journeys.reduce((totalDistance, journey) => (totalDistance + journey.distance), 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
